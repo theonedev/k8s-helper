4b035e7e9e00a8bccc4d3d8c4cf7583b1b3a0556
Robin Shen <robin@pmease.com>
2019-10-29 20:37:23 +0800

    Add batch file to generate/upload windows images

---
+++ build.bat
@@ -0,0 +1,11 @@
+@echo off 
+cmd /c mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout > version.txt || cat version.txt && rm version.txt && exit /b 1
+set /p buildVersion=<version.txt
+rm version.txt
+
+cmd /c mvn clean package || exit /b 1
+
+for %%i in (1607,1709,1803,1809,1903) do (
+  docker build -t 1dev/k8s-helper-windows-%%i:%buildVersion% -f Dockerfile.windows --build-arg osVersion=%%i . || exit /b 1
+  docker push 1dev/k8s-helper-windows-%%i:%buildVersion% || exit /b 1
+)
