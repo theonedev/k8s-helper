11762852430561d1cef27f0d7480e5d9ff59e216
Robin Shen <robin@onedev.io>
2019-10-29 10:14:31 +0800

    fail fast in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -e
 buildVersion=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)
 mvn clean package
 docker build -t 1dev/k8s-helper-linux:$buildVersion -f Dockerfile.linux .
