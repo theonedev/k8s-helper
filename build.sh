69e3ca49a3e5e73f6b3c896a1bc76a9a501399ac
Robin Shen <robin@pmease.com>
2019-12-24 17:33:35 +0800

    docker hub login first in build.sh to exit quickly upon errors

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e
-if [ $# -eq 0 ]
-  then
-    echo "Docker hub password should be supplied as argument"
-    exit 1
-fi
+docker login -u robinshen -p $@
 buildVersion=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)
 mvn clean package
 docker build -t 1dev/k8s-helper-linux:$buildVersion -f Dockerfile.linux .
-docker login -u robinshen -p $@
 docker push 1dev/k8s-helper-linux:$buildVersion
