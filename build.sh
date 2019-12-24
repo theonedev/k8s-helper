82fc26f662a63e5da36b436fd9afcb2793a33214
Robin Shen <robin@pmease.com>
2019-12-24 17:27:12 +0800

    Check docker hub password

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
+if [ $# -eq 0 ]
+  then
+    echo "Docker hub password should be supplied as argument"
+    exit 1
+fi
 buildVersion=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)
 mvn clean package
 docker build -t 1dev/k8s-helper-linux:$buildVersion -f Dockerfile.linux .
