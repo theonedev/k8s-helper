842b9e4cc1287630751ccbeac0247c18d9f12b58
Robin Shen <robin@onedev.io>
2023-08-24 21:05:00 +0800

    feat: arm64 support

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 set -e
+
 docker login -u robinshen -p $@
 buildVersion=$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)
 mvn clean package
-docker build -t 1dev/k8s-helper-linux:$buildVersion -f Dockerfile.linux .
-docker push 1dev/k8s-helper-linux:$buildVersion
+
+BUILDER="builder-$(date +%s)"
+
+function finish {
+  docker buildx stop $BUILDER
+  docker buildx rm $BUILDER
+}
+trap finish EXIT
+
+docker buildx create --name $BUILDER
+docker buildx use $BUILDER
+
+docker buildx build --push --platform linux/amd64,linux/arm64 -f ./Dockerfile -t 1dev/k8s-helper-linux:$buildVersion .
