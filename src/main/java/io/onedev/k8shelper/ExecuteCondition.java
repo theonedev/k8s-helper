f9e2b16030dd76a5e2f8c832d03b1b30ff087e46
Robin Shen <robin@onedev.io>
2025-05-14 11:48:23 +0800

    wip: i18n support

--- src/main/java/io/onedev/k8shelper/ExecuteCondition.java
+++ src/main/java/io/onedev/k8shelper/ExecuteCondition.java
@@ -1,13 +1,7 @@
 package io.onedev.k8shelper;
 
-import io.onedev.commons.utils.StringUtils;
-
 public enum ExecuteCondition {
 
 	ALL_PREVIOUS_STEPS_WERE_SUCCESSFUL, ALWAYS, NEVER;
-
-	public String getDisplayName() {
-		return StringUtils.capitalize(name().replace('_', ' ').toLowerCase());
-	}
-	
+		
 }
